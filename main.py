d261c747c69646e0dead783cb6db01b463c9023d
ShaikAbbuSofiyan <167667132+ShaikAbbuSofiyan@users.noreply.github.com>
2025-03-21 12:45:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
+# import streamlit as st
+
+
+# st.title('Hello World')
+# st.write('This is a simple Streamlit app.')
+
+
+# if st.button('Say hello'):
+#    st.text('Hello, Streamlit!')
+
+
+# name = st.text_input('Please enter your name:')
+# if name:
+#    st.write(f'Hello, {name}!')
+
+
+# if st.file_uploader('Please upload a file:', type=['txt', 'csv']):
+#    st.write('Thanks for uploading a file!')
 import streamlit as st
 
+# st.title("New application")
 
-st.title('Hello World')
-st.write('This is a simple Streamlit app.')
+name = st.text_input("Enter your name", "Sofiyan")
+password = st.text_input("Enter your password", type= "password")
 
-
-if st.button('Say hello'):
-   st.text('Hello, Streamlit!')
-
-
-name = st.text_input('Please enter your name:')
-if name:
-   st.write(f'Hello, {name}!')
-
-
-if st.file_uploader('Please upload a file:', type=['txt', 'csv']):
-   st.write('Thanks for uploading a file!')
+if st.button("Submit"):
+    if len(password) < 8:
+        st.warning("Password must be at least 8 characters long")
+    st.success("Password is valid")
